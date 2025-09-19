0c2b26a313580671974832aefdf88c4ad6e32457
Farooqfar <info4895@gmail.com>
2025-09-19 16:49:02 +0500

    co

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,10 @@ export default function RootLayout({ children }) {
 
           <Nav />
           <div className="flex justify-center items-center gap-1">
-            <Button className="text-3xl" text={<CiSearch />} />
+            <Button
+              className="text-3xl cursor-pointer hover:scale-95 transition-all duration-200"
+              text={<CiSearch />}
+            />
             <Link href="/login">
               <Button
                 className="text-3xl font-light cursor-pointer hover:scale-95 transition-all duration-200"
@@ -30,9 +33,12 @@ export default function RootLayout({ children }) {
               />
             </Link>
             <div className="flex justify-center items-center">
-              <Button className="text-3xl " text={<CiShoppingCart />} />
               <Button
-                className="bg-black text-white text-sm w-5 rounded-full"
+                className="text-3xl cursor-pointer hover:scale-95 transition-all duration-200"
+                text={<CiShoppingCart />}
+              />
+              <Button
+                className="bg-black text-white text-sm w-5 rounded-full  hover:scale-95 transition-all duration-200"
                 text="1"
               />
             </div>
